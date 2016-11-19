38ff655d04f2d71d3f90da6722007da2266f9361
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2016-11-19 11:44:23 -0500

    add initial search box

--- Main.elm
+++ Main.elm
@@ -1,3 +1,57 @@
-import Html
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
-main = Html.text "hello world"
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { searchText : String
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model "", Cmd.none)
+
+
+-- UPDATE
+
+type Msg
+  = SearchText String
+  | Search
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    SearchText text ->
+      ({ model | searchText = text }, Cmd.none)
+    Search ->
+      (model, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Search Text", onInput SearchText] []
+    , button [ onClick Search ] [ text "search" ]
+    , div [] [text ("Searching for... " ++ model.searchText)]
+    ]
