b9755b6c2bae17b862476d79b45ed1b5447c6a9a
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2016-11-19 00:38:55 -0500

    config Elm support

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
+const chokidar = require('chokidar')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+
+chokidar.watch(['ports.js', 'index.html', 'elm.js']).on('change', () => {
+  if (win) {
+    win.reload()
+  }
+})
 
 function createWindow () {
   // Create the browser window.
