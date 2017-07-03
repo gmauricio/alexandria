c1feedce5cc458da50e294d4d8758914a84323d1
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2017-07-03 14:18:18 -0500

    add some simple error handling and encode text string to utf8

--- routes/search.js
+++ routes/search.js
@@ -1,4 +1,5 @@
 const request = require('request');
+var utf8 = require('utf8');
 
 module.exports = function(app, db) {
 	app.use(function(req, res, next) {
@@ -10,10 +11,18 @@ module.exports = function(app, db) {
   app.post('/search', (req, res) => {
 		const text = req.body.text;
 		const host = "72.191.219.159";
-		const url = "http://" + host + "/mobile?num=9999999&search=" + text +  "&sort=title&order=ascending";
+		const url = "http://" + host + "/mobile?num=9999999&search=" + utf8.encode(text) +  "&sort=title&order=ascending";
 		request.get(url,null, (err, response, html) => {
-			if (err) console.log(err);
-			if (response.statusCode === 200 ) res.json([ { host, html } ]);
+			if (err) {
+				console.log(err);
+				res.status(500).json({ message: err.message })
+			}
+			if (response.statusCode === 200 ) {
+				res.json([ { host, html } ])
+			} else {
+				console.log('status', response.statusCode);
+				res.status(500).json({ message: 'Unknown error' })
+			}
 		});
   });
 };
