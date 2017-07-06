a94f642b2f900878a024ea68f4510f42614f4a73
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2017-07-06 09:17:29 -0500

    serve static public files with express

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 8000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+app.use(express.static('public'))
+
 require('./routes')(app, {});
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log('We are live on ' + port);
 })
